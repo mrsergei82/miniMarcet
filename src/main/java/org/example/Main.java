b123c6ef358df5684d43b2feeb3c3c7a15db9966
Sergei <mr.sergei82@mail.ru>
2022-01-16 15:16:08 +0300

    добавилБДпока не работает

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,39 @@
 package org.example;
 
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import ru.geekbrains.java4.lesson6.db.dao.ProductsMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+@UtilityClass
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private final String resource = "mybatis-config.xml";
+    public static SqlSession main() throws IOException {
         SqlSessionFactory sqlSessionFactory;
         //NyBatis Configuration file
-        String resource = "mybatisConfig.xml";
+        //String resource = "mybatis-config.xml";
         InputStream is = Resources.getResourceAsStream(resource);
         sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        return sqlSession;
+
+    }
+
+    @SneakyThrows
+    public<T> T  getCategoriMaper(Class<T>tClass)
+    {
+        return new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(resource))
+                .openSession()
+                .getMapper(tClass);
+    }
+    @SneakyThrows
+    public ProductsMapper getProductMaper(){
+        return main().getMapper(ProductsMapper.class);
     }
 }
--- src/test/java/org/example/CreateProductTest.java
+++ src/test/java/org/example/CreateProductTest.java
@@ -2,19 +2,19 @@ package org.example;
 import com.github.javafaker.Faker;
 import lombok.SneakyThrows;
 import okhttp3.ResponseBody;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.hamcrest.*;
 import org.junit.jupiter.api.*;
 import retrofit2.Response;
-
-
 import org.example.util.RetrofitUtils;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.example.dto.Product;
 import org.example.service.ProductService;
-
 import org.junit.jupiter.api.BeforeAll;
+import ru.geekbrains.java4.lesson6.db.dao.ProductsMapper;
 
 public class CreateProductTest {
     static ProductService productService;
@@ -38,44 +38,42 @@ public class CreateProductTest {
     @Test
     @SneakyThrows
     void createProductInFoodCategoryTest(){
+
         Response<Product> response = productService.createProduct(product)
                 .execute();
         id= response.body().getId();
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
+
+
     }
     @Test
     @SneakyThrows
     void getProdTest(){
         currentId = productService.createProduct(product).execute().body().getId();
-        Response<Product> response = productService.getProduct(currentId)
+        Response<Product> response = productService.getProduct((int) currentId)
                 .execute();
         id=currentId;
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
     }
     @Test
     @SneakyThrows
-    void putProductTest(){
+    void putProductTest() throws Exception{
         currentId = productService.createProduct(product).execute().body().getId();
-        System.out.println(currentId);
-
         Product newProduct = new Product()
-                .withId(currentId)
+                .withId((int)currentId)
                 .withTitle("Bread")
                 .withPrice((int) 500)
                 .withCategoryTitle("Food");
-        System.out.println(newProduct);
         Response<Product> response = productService.modifyProduct(newProduct).execute();
-        System.out.println(response);
         id=currentId;
         assertThat(response.isSuccessful(),CoreMatchers.is(true));
-
+        ProductsMapper pm = Main.getProductMaper();
+        pm.selectByPrimaryKey((long)response.body().getId()).equals(currentId);
     }
-
-
     @SneakyThrows
     @AfterEach
     void tearDown(){
-        Response<ResponseBody> response = productService.deleteProduct(id).execute();
+        Response<ResponseBody> response = productService.deleteProduct((int) id).execute();
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
     }
 }
